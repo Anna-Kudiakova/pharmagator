19684e0cb8db0731ef83bd0b24bae8f9461cb381
Anna <ann.kudiakova@gmail.com>
2021-10-06 21:25:58 +0300

    Revert "Failure comment"
    This reverts commit 1e30ecdd315d9d2c7a08301178ec7b6301672ec5.

--- api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
+++ api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
@@ -3,15 +3,12 @@ package com.eleks.academy.pharmagator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 @SpringBootTest
 class PharmagatorApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertEquals(1,9);
 	}
 
 }
