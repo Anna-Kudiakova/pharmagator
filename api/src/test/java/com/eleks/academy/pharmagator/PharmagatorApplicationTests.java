8a09ef309790eb1e79cf5e1331941914af85b0b0
Anna <ann.kudiakova@gmail.com>
2021-10-11 00:26:45 +0300

    Added annotation @ActiveProfiles to api\test

--- api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
+++ api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
@@ -2,11 +2,13 @@ package com.eleks.academy.pharmagator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PharmagatorApplicationTests {
 
 	@Test
