c74870828498a4527eef3fa87efbd01a4410e779
Anna <ann.kudiakova@gmail.com>
2021-11-11 21:34:54 +0200

    add flyway migration config

---
+++ api/src/main/java/com/eleks/academy/pharmagator/config/FlywayMigrationConfig.java
@@ -0,0 +1,17 @@
+package com.eleks.academy.pharmagator.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayMigrationConfig {
+
+    @Bean
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+}
